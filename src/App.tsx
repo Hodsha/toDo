fa1bfb081883f1cc9c6dd302bcf29abe0b0e56e4
Hodsha <88711400+Hodsha@users.noreply.github.com>
2023-05-29 13:36:30 +0200

    Todos

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     Todo erstellen
+      Todo eingeben 
+      nach Datum sortieren
+      nach wichtigkeit sortieren
+      man kann auch ältere todos anschauen
+      wenn erledigt anklicken
+      wenn nicht erledigt, dann rot anzeigen
+      Entweder in Datenbank speichern oder in session store
+
+     
     </>
   )
 }
